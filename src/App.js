6631b41fa2402d388df68a0440713a00c386c718
Yevhenii Skrypnyk <y.skrypnyk@astoundcommerce.com>
2019-05-24 13:44:18 +0300

    finished Person component

--- src/App.js
+++ src/App.js
@@ -10,47 +10,65 @@ class App extends Component {
       { name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value'
-  }
+  };
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // Don't: this.state.persons[0].name = 'Maximilian';
     this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
+        { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
+  };
+
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
     });
   }
 
   render() {
+    const style = {
+      backgroundColor: '#fff',
+      font: 'inherit',
+      border: '1px solid lightblue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button
+          onClick={this.switchNameHandler.bind(this, 'Maximilian')}
+          style={style}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
+          age={this.state.persons[0].age} />
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-        >
-          My Hobbies: Racing
-        </Person>
+          click={this.switchNameHandler.bind(this, 'Maximilian')}
+          changed={this.nameChangeHandler}>My Hobbies: Racing</Person>
         <Person
           name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+          age={this.state.persons[2].age} />
       </div>
     );
 
     // return React.createElement('div', {className: 'App'},
     //   React.createElement('h1', null, 'it\'s h1 title')
     // );
-  }
-}
+  };
+};
 
 export default App;
